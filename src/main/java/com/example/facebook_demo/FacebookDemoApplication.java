b037175daca7ee46c40900f0f93b63c63cd06e69
waddipoojitha <waddipoojitha24@gmail.com>
2025-07-18 15:53:27 +0530

    first commit

---
+++ src/main/java/com/example/facebook_demo/FacebookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.facebook_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookDemoApplication.class, args);
+	}
+
+}
